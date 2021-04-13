395f723564a314daa6e00bacc59bd87c6e407795
Abhishek Goenka <abhishek.goenka@epicor.com>
2021-04-13 11:57:00 +0530

    Add city

--- client/src/app/city-edit/city-edit.component.ts
+++ client/src/app/city-edit/city-edit.component.ts
@@ -1,8 +1,9 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
 import { FormControl, FormGroup } from "@angular/forms";
 import { ActivatedRoute, Router } from "@angular/router";
 import { City } from "../cities/city";
+import { Country } from "../countries/country";
 
 @Component({
   selector: "app-city-edit",
@@ -16,6 +17,12 @@ export class CityEditComponent implements OnInit {
   form: FormGroup;
   // the city object to edit
   city: City;
+  // the city object id, as fetched from the active route:
+  // It's NULL when we're adding a new city,
+  // and not NULL when we're editing an existing one.
+  id?: number;
+  // the countries array for the select
+  countries: Country[];
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -28,39 +35,82 @@ export class CityEditComponent implements OnInit {
       name: new FormControl(""),
       lat: new FormControl(""),
       lon: new FormControl(""),
+      countryId: new FormControl(""),
     });
     this.loadData();
   }
 
   loadData() {
-    // retrieve the ID from the 'id' parameter
-    var id = +this.activatedRoute.snapshot.paramMap.get("id");
-    // fetch the city from the server
-    var url = "api/Cities/" + id;
-    this.http.get<City>(url).subscribe(
-      (result) => {
-        this.city = result;
-        this.title = "Edit - " + this.city.name;
-        // update the form with the city value
-        this.form.patchValue(this.city);
-      },
-      (error) => console.error(error)
-    );
+    // load countries
+    this.loadCountries();
+
+    // retrieve the ID from the 'id'
+    this.id = +this.activatedRoute.snapshot.paramMap.get("id");
+    if (this.id) {
+      // EDIT MODE
+      // fetch the city from the server
+      var url = "api/Cities/" + this.id;
+      this.http.get<City>(url).subscribe(
+        (result) => {
+          this.city = result;
+          this.title = "Edit - " + this.city.name;
+          // update the form with the city value
+          this.form.patchValue(this.city);
+        },
+        (error) => console.error(error)
+      );
+    } else {
+      // ADD NEW MODE
+      this.title = "Create a new City";
+    }
   }
 
   onSubmit() {
-    var city = this.city;
+    var city = this.id ? this.city : <City>{};
     city.name = this.form.get("name").value;
     city.lat = +this.form.get("lat").value;
     city.lon = +this.form.get("lon").value;
-    var url = "api/Cities/" + this.city.id;
-    this.http.put<City>(url, city).subscribe(
-      (result) => {
-        console.log("City " + city.id + " has been updated.");
-        // go back to cities view
-        this.router.navigate(["/cities"]);
-      },
-      (error) => console.error(error)
-    );
+    city.countryId = +this.form.get("countryId").value;
+    
+    if (this.id) {
+      // EDIT mode
+      var url = "api/Cities/" + this.city.id;
+      this.http.put<City>(url, city).subscribe(
+        (result) => {
+          console.log("City " + city.id + " has been updated.");
+          // go back to cities view
+          this.router.navigate(["/cities"]);
+        },
+        (error) => console.error(error)
+      );
+    } else {
+      // ADD NEW mode
+      var url = "api/Cities";
+      this.http.post<City>(url, city).subscribe(
+        (result) => {
+          console.log("City " + result.id + " has been created.");
+          // go back to cities view
+          this.router.navigate(["/cities"]);
+        },
+        (error) => console.error(error)
+      );
+    }
+  }
+
+  loadCountries() {
+    // fetch all the countries from the server
+    var url = "api/Countries";
+    var params = new HttpParams()
+      .set("pageIndex", "0")
+      .set("pageSize", "9999")
+      .set("sortColumn", "name");
+    this.http
+      .get<any>(url, { params })
+      .subscribe(
+        (result) => {
+          this.countries = result.data;
+        },
+        (error) => console.error(error)
+      );
   }
 }
--- client/src/app/material.module.ts
+++ client/src/app/material.module.ts
@@ -3,9 +3,10 @@ import { MatPaginatorModule } from "@angular/material/paginator";
 import { MatSortModule } from "@angular/material/sort";
 import { MatTableModule } from "@angular/material/table";
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from "@angular/material/select";
 
 @NgModule({
-  imports: [MatTableModule, MatPaginatorModule, MatSortModule, MatInputModule],
-  exports: [MatTableModule, MatPaginatorModule, MatSortModule, MatInputModule],
+  imports: [MatTableModule, MatPaginatorModule, MatSortModule, MatInputModule,MatSelectModule],
+  exports: [MatTableModule, MatPaginatorModule, MatSortModule, MatInputModule, MatSelectModule],
 })
 export class AngularMaterialModule {}
